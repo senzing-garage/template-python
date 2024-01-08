8cad79879bc5d5cb7554dfecc460a20cf9a886d8
Michael Dockter <michael@dockter.com>
2024-01-08 08:52:21 -0500

    #84 Updates for migrating repositories (#86)

---
+++ makefiles/darwin.mk
@@ -0,0 +1,55 @@
+# Makefile extensions for darwin.
+
+# -----------------------------------------------------------------------------
+# Variables
+# -----------------------------------------------------------------------------
+
+SENZING_DIR ?= /opt/senzing/g2
+SENZING_TOOLS_SENZING_DIRECTORY ?= $(SENZING_DIR)
+
+LD_LIBRARY_PATH := $(SENZING_TOOLS_SENZING_DIRECTORY)/lib:$(SENZING_TOOLS_SENZING_DIRECTORY)/lib/macos
+DYLD_LIBRARY_PATH := $(LD_LIBRARY_PATH)
+
+# -----------------------------------------------------------------------------
+# OS specific targets
+# -----------------------------------------------------------------------------
+
+.PHONY: clean-osarch-specific
+clean-osarch-specific:
+	@docker rm --force $(DOCKER_CONTAINER_NAME) 2> /dev/null || true
+	@docker rmi --force $(DOCKER_IMAGE_NAME) $(DOCKER_BUILD_IMAGE_NAME) 2> /dev/null || true
+	@rm -rf $(TARGET_DIRECTORY) || true
+	@rm -f $(GOPATH)/bin/$(PROGRAM_NAME) || true
+
+
+.PHONY: hello-world-osarch-specific
+hello-world-osarch-specific:
+	@echo "Hello World, from darwin."
+
+
+.PHONY: package-osarch-specific
+package-osarch-specific:
+	@echo No packaging for darwin.
+
+
+.PHONY: run-osarch-specific
+run-osarch-specific:
+	@go run -exec macos_exec_dyld.sh main.go
+
+
+.PHONY: setup-osarch-specific
+setup-osarch-specific:
+	@echo "No setup required."
+
+
+.PHONY: test-osarch-specific
+test-osarch-specific:
+	@go test -exec macos_exec_dyld.sh -v -p 1 ./...
+
+# -----------------------------------------------------------------------------
+# Makefile targets supported only by this platform.
+# -----------------------------------------------------------------------------
+
+.PHONY: only-darwin
+only-darwin:
+	@echo "Only darwin has this Makefile target."
---
+++ makefiles/windows.mk
@@ -0,0 +1,48 @@
+# Makefile extensions for windows.
+
+# -----------------------------------------------------------------------------
+# Variables
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# OS specific targets
+# -----------------------------------------------------------------------------
+
+.PHONY: clean-osarch-specific
+clean-osarch-specific:
+	del /F /S /Q $(TARGET_DIRECTORY)
+	del /F /S /Q $(GOPATH)/bin/$(PROGRAM_NAME)
+
+
+.PHONY: hello-world-osarch-specific
+hello-world-osarch-specific:
+	@echo "Hello World, from windows."
+
+
+.PHONY: package-osarch-specific
+package-osarch-specific:
+	@echo No packaging for windows.
+
+
+.PHONY: run-osarch-specific
+run-osarch-specific:
+	@go run main.go
+
+
+.PHONY: setup-osarch-specific
+setup-osarch-specific:
+	@echo "No setup required."
+
+
+.PHONY: test-osarch-specific
+test-osarch-specific:
+	@go test -v -p 1 ./...
+
+# -----------------------------------------------------------------------------
+# Makefile targets supported only by this platform.
+# -----------------------------------------------------------------------------
+
+.PHONY: only-windows
+only-windows:
+	@echo "Only windows has this Makefile target."
