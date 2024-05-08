8595792e696a277ecbff04a1f57eab56e93853ac
Sam <109683132+kernelsam@users.noreply.github.com>
2024-05-08 15:24:46 -0700

    add sample test

---
+++ tests/sample_test.py
@@ -0,0 +1,11 @@
+"""Placeholder for defining test functions."""
+
+
+def func(x):
+    """Function to increment by 1."""
+    return x + 1
+
+
+def test_answer():
+    """Validate func return val."""
+    assert func(3) == 4
